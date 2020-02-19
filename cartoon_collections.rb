9942923c24259033f530d991b7bab897d659bc8e
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 17:08:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(snack)
-    def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |a| cheese_types.include?(a) }
 end
